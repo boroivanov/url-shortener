14bed906ef3cf161f4210275180fdf5addd5efae
Borislav Ivanov <borogl@gmail.com>
2016-09-09 21:37:36 -0700

    Lambda IAM Role and Policy

--- infrastructure/url-shortener.tf
+++ infrastructure/url-shortener.tf
@@ -37,3 +37,64 @@ resource "aws_dynamodb_table" "url-shortener-tokens" {
     type = "S"
   }
 }
+
+# Lambda IAM Role
+resource "aws_iam_role" "url-shortener-lambda-role" {
+    name = "url-shortener-lambda-role"
+    assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+# Lambda IAM Policy - CloudWatch Logs, DynamoDB, and KMS
+resource "aws_iam_role_policy" "url-shortener-lambda-policy" {
+    name = "url-shortener-lambda-policy"
+    role = "${aws_iam_role.url-shortener-lambda-role.id}"
+    policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Action": [
+        "kms:Encrypt",
+        "kms:Decrypt",
+        "kms:DescribeKey",
+        "kms:GetKeyPolicy"
+      ],
+      "Resource": [
+        "arn:aws:kms:*:*:alias/url-shortener"
+      ],
+      "Effect": "Allow"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "dynamodb:*"
+      ],
+      "Resource": "arn:aws:dynamodb:*:*:table/url-shortener*"
+    }
+  ]
+}
+EOF
+}
